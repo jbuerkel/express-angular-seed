e4ca12988df07deab1a3d2319ca195aa0d8f9d23
Jonas Bürkel <buerkelj@gmail.com>
2016-06-16 11:24:32 +0200

    feat(bin): add https setup

---
+++ cert.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+if [ ! -d 'cert' ]; then
+    mkdir cert
+    openssl req -x509 -sha256 -newkey rsa:2048 -keyout cert/key.pem -out cert/cert.pem -days 90 -nodes -subj '//CN=localhost'
+else
+    echo 'Certificate folder already exists'
+fi
